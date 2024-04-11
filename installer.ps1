c530880551e521634dc4a820fa2238694a49c527
tinitiateprime <118025881+tinitiateprime@users.noreply.github.com>
2024-04-11 09:30:44 +0530

    Create installer.ps1

---
+++ installer.ps1
@@ -0,0 +1,23 @@
+# Check if Node.js is installed
+if (-not (Get-Command "node" -ErrorAction SilentlyContinue)) {
+    Write-Host "Node.js is not installed. Installing Node.js..."
+    choco install nodejs -y
+}
+
+# Initialize a Node.js project
+npm init -y
+
+# Define the modules to be installed
+$modules = @("express", "markdown-it", "clipboardy", "aws-sdk", "express-session", "dotenv", "axios")
+
+# Iterate over each module and install it if it's not already installed
+foreach ($module in $modules) {
+    # Check if the module is already installed
+    $modulePath = Join-Path "node_modules" $module
+    if (Test-Path $modulePath) {
+        Write-Host "$module is already installed."
+    } else {
+        # Install the module
+        npm install $module
+    }
+}
